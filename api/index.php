e9c15ae9cbd653d6646d084d55ada521839e7abf
mbojostudio <anharaldevaro789@gmail.com>
2025-12-29 04:02:17 +0800

    first commit

--- api/index.php
+++ api/index.php
@@ -1,16 +1,9 @@
 <?php
 
-// 1. Tampilkan error agar kita bisa debug jika masih gagal
-
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
-
-// 2. Paksa Laravel menggunakan folder /tmp untuk cache views
-// Karena folder storage asli di Vercel bersifat Read-Only
+// Paksa Laravel menggunakan folder /tmp untuk cache views karena storage asli Read-Only
 putenv('VIEW_COMPILED_PATH=/tmp/storage/framework/views');
 
-// 3. Buat folder tmp jika belum ada (opsional tapi disarankan)
+// Buat folder tmp secara otomatis
 if (!is_dir('/tmp/storage/framework/views')) {
     mkdir('/tmp/storage/framework/views', 0755, true);
 }
