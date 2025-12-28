f0ace240b238dd3cd8c5d6d07e83d2b22cabc8a0
mbojostudio <anharaldevaro789@gmail.com>
2025-12-29 04:41:38 +0800

    first commit

--- api/index.php
+++ api/index.php
@@ -1,9 +1,9 @@
 <?php
 
 // Paksa folder view ke /tmp (Wajib di Vercel)
-putenv('VIEW_COMPILED_PATH=/tmp');
-if (!is_dir('/tmp')) {
-    mkdir('/tmp', 0755, true);
+putenv('VIEW_COMPILED_PATH=/tmp/views');
+if (!is_dir('/tmp/views')) {
+    mkdir('/tmp/views', 0755, true);
 }
 
 // Pastikan Laravel tidak mencoba menggunakan file untuk maintenance mode
