25abca6df00e2965d72ab02b0296dd9655c39ecc
mbojostudio <anharaldevaro789@gmail.com>
2025-12-29 07:36:16 +0800

    first commit

--- config/cache.php
+++ config/cache.php
@@ -49,8 +49,8 @@ return [
 
         'file' => [
             'driver' => 'file',
-            'path' => storage_path('framework/cache/data'),
-            'lock_path' => storage_path('framework/cache/data'),
+            'path' => storage_path('tmp/storage/framework/cache/data'),
+            'lock_path' => storage_path('tmp/storageframework/cache/data'),
         ],
 
         'memcached' => [
