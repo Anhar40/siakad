f5ae14506dfad854d91794c259876b2e66210f94
mbojostudio <anharaldevaro789@gmail.com>
2025-12-29 04:39:11 +0800

    first commit

--- api/index.php
+++ api/index.php
@@ -1,4 +1,12 @@
 <?php
 
+// Paksa folder view ke /tmp (Wajib di Vercel)
+putenv('VIEW_COMPILED_PATH=/tmp');
+if (!is_dir('/tmp')) {
+    mkdir('/tmp', 0755, true);
+}
+
+// Pastikan Laravel tidak mencoba menggunakan file untuk maintenance mode
+putenv('APP_MAINTENANCE_DRIVER=array');
 
 require __DIR__ . '/../public/index.php';
