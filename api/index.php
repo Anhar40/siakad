3c4c0148c84fd0bfd8a99eb0d07df52c8c65f9c6
mbojostudio <anharaldevaro789@gmail.com>
2025-12-29 04:31:29 +0800

    first commit

--- api/index.php
+++ api/index.php
@@ -1,24 +1,4 @@
 <?php
 
-// 1. Matikan cache konfigurasi & rute agar Laravel membaca .env terbaru
-putenv('APP_CONFIG_CACHE=/tmp/config.php');
-putenv('APP_ROUTES_CACHE=/tmp/routes.php');
-putenv('APP_SERVICES_CACHE=/tmp/services.php');
-putenv('APP_PACKAGES_CACHE=/tmp/packages.php');
-
-// 2. Pastikan folder view bisa ditulis di folder temporary Vercel
-putenv('VIEW_COMPILED_PATH=/tmp/storage/framework/views');
-
-// 3. Buat folder yang diperlukan jika belum ada
-$folders = [
-    '/tmp/storage/framework/views',
-    '/tmp/storage/framework/sessions',
-    '/tmp/storage/framework/cache',
-];
-foreach ($folders as $folder) {
-    if (!is_dir($folder)) {
-        mkdir($folder, 0755, true);
-    }
-}
 
 require __DIR__ . '/../public/index.php';
