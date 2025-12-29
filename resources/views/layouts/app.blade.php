db2ce5093cd01266fb60e01f44e02ad4af1f2c2b
Anhar <152875918+Anhar40@users.noreply.github.com>
2025-12-29 10:48:45 +0800

    Update app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,7 +7,7 @@
 
         @php
             $routeName = request()->route()?->getName() ?? 'default';
-            $logo = asset('og/LOGO_KAMPUS.png');
+            $logo = asset('og/LOGO_KAMPUS.jpg');
             // Default metadata
             $og = [
                 'title' => 'SIAKAD - Sistem Informasi Akademik',
