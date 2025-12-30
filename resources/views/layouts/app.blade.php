4260d2a4afd0b248d008927b9e6e0ca0984849b4
mbojostudio <anharaldevaro789@gmail.com>
2025-12-30 18:00:39 +0800

    first commit

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -80,7 +80,7 @@
     <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
     <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
     <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
-    <link rel="manifest" href="/manifest.json">
+    <link rel="manifest" href="/site.webmanifest">
     <meta rel="icon" href="/favicon-32x32.ico" sizes="32x32">
     <meta name="theme-color" content="#fefeff" id="theme-color-meta">
     {{-- Primary Meta Tags --}}
