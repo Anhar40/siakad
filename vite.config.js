e9b6124123adc573b76a58947b572af4614bc1b3
mbojostudio <anharaldevaro789@gmail.com>
2025-12-30 15:42:45 +0800

    first commit

--- vite.config.js
+++ vite.config.js
@@ -16,7 +16,7 @@ export default defineConfig({
                 theme_color: '#0d6efd',
                 background_color: '#ffffff',
                 display: 'standalone',
-                start_url: '/login',
+                start_url: '/',
                 icons: [
                     {
                         src: '/android-chrome-192x192.png',
