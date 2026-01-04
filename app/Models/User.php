af1a58dadf50e495339ff7c41d43ade3a7b681b2
mbojostudio <anharaldevaro789@gmail.com>
2026-01-05 06:53:45 +0800

    first commit

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,6 +6,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Spatie\Permission\Traits\HasRoles;
+use Illuminate\Support\Str;
 
 class User extends Authenticatable
 {
