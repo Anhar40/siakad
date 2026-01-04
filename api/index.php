9606810e204f015a0407562b2b69f389988fca73
mbojostudio <anharaldevaro789@gmail.com>
2026-01-05 07:53:58 +0800

    first commit

--- api/index.php
+++ api/index.php
@@ -22,7 +22,7 @@ putenv("SESSION_DRIVER=cookie"); // Paling aman untuk Vercel
 putenv("CACHE_STORE=array");      // Gunakan 'file', bukan 'array'
 putenv("CACHE_DIRECTORY={$storagePath}/framework/cache/data");
 putenv("APP_CONFIG_CACHE={$storagePath}/bootstrap/cache/config.php");
-putenv("APP_DEBUG=true");        // Biarkan true dulu untuk pantau error berikutnya
+putenv("APP_DEBUG=false");        // Biarkan true dulu untuk pantau error berikutnya
 
 // 4. Jalankan aplikasi
 require $basePath . '/public/index.php';
