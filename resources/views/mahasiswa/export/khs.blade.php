21b4f957d86ec5945356d04a6ca6f63828b3f111
mbojostudio <anharaldevaro789@gmail.com>
2026-01-06 16:46:17 +0800

    firt commit

--- resources/views/mahasiswa/export/khs.blade.php
+++ resources/views/mahasiswa/export/khs.blade.php
@@ -324,7 +324,7 @@
             {{ $mahasiswa->prodi->fakultas->nama ?? '-' }}<br>
             Ketua Program Studi {{ $mahasiswa->prodi->nama ?? '-' }}
             <div class="nama">{{ $mahasiswa->prodi->ketua_prodi ?? '-' }}</div>
-            NIDN. {{ $mahasiswa->prodi->ketua_prodi->nidn ?? '-' }}
+            NIDN. {{ $mahasiswa->prodi->nidn ?? '-' }}
         </div>
     </div>
 
