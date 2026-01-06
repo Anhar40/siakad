13c16515cc5ad65b22873fdd85fa2f9219ff6eba
mbojostudio <anharaldevaro789@gmail.com>
2026-01-06 16:03:09 +0800

    firt commit

--- resources/views/mahasiswa/export/khs.blade.php
+++ resources/views/mahasiswa/export/khs.blade.php
@@ -287,7 +287,7 @@
 
     <br>
     @php
-        $ipk = $ipkData['ipk'] ?? 0; // Pastikan ada nilai default jika data kosong
+        $ipk = {{ number_format($ipsData['ips'], 2) }}; // Pastikan ada nilai default jika data kosong
         $maxSks = 0;
     
         if($ipk >= 3.00) {
